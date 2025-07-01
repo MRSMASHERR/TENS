6b964145ea1bb687e0db77f7054518f031856477
MRSMASHERR <nicolas_velasquez133@outlook.es>
2025-07-01 18:10:01 -0400

    Fix: Remove unused dj_database_url import

--- sistema_medico/settings.py
+++ sistema_medico/settings.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 from pathlib import Path
-import dj_database_url  # <-- Asegúrate de tenerlo en requirements.txt
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
